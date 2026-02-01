513054674574371ddc4f779c607757a639b5545d
Bharat Goyal <bharat.goyal1@1mg.com>
2026-02-01 20:38:28 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Configure this properly in production
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
